f5534b96da454a213794f09669bc08e9e7194580
thanhtuanapril1 <thanhtuan.april1@gmail.com>
2024-07-21 10:17:05 +0700

    Add project files.

---
+++ ConnectOracleDatabase/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btView = new System.Windows.Forms.Button();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblRegNo = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbRegNo = new System.Windows.Forms.TextBox();
+            this.tbStudentName = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(801, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btView
+            // 
+            this.btView.Location = new System.Drawing.Point(43, 415);
+            this.btView.Name = "btView";
+            this.btView.Size = new System.Drawing.Size(75, 23);
+            this.btView.TabIndex = 1;
+            this.btView.Text = "View";
+            this.btView.UseVisualStyleBackColor = true;
+            this.btView.Click += new System.EventHandler(this.btView_Click);
+            // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(158, 415);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(75, 23);
+            this.btInsert.TabIndex = 1;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(273, 415);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.TabIndex = 1;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(40, 333);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID";
+            // 
+            // lblRegNo
+            // 
+            this.lblRegNo.AutoSize = true;
+            this.lblRegNo.Location = new System.Drawing.Point(189, 333);
+            this.lblRegNo.Name = "lblRegNo";
+            this.lblRegNo.Size = new System.Drawing.Size(44, 13);
+            this.lblRegNo.TabIndex = 2;
+            this.lblRegNo.Text = "Reg No";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(342, 333);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(75, 13);
+            this.lblStudentName.TabIndex = 2;
+            this.lblStudentName.Text = "Student Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(490, 333);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 2;
+            this.lblAddress.Text = "Address";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(43, 349);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 20);
+            this.tbID.TabIndex = 3;
+            // 
+            // tbRegNo
+            // 
+            this.tbRegNo.Location = new System.Drawing.Point(192, 349);
+            this.tbRegNo.Name = "tbRegNo";
+            this.tbRegNo.Size = new System.Drawing.Size(100, 20);
+            this.tbRegNo.TabIndex = 3;
+            // 
+            // tbStudentName
+            // 
+            this.tbStudentName.Location = new System.Drawing.Point(345, 349);
+            this.tbStudentName.Name = "tbStudentName";
+            this.tbStudentName.Size = new System.Drawing.Size(100, 20);
+            this.tbStudentName.TabIndex = 3;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(493, 349);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(100, 20);
+            this.tbAddress.TabIndex = 3;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(382, 415);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 4;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbStudentName);
+            this.Controls.Add(this.tbRegNo);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblRegNo);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.btView);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMySQL_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btView;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblRegNo;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbRegNo;
+        private System.Windows.Forms.TextBox tbStudentName;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
---
+++ ConnectOracleDatabase/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using Oracle.ManagedDataAccess.Client;
+using System.Security.Cryptography;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //Initial variable
+        static string connStr = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))" +
+            "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=OTMVN)));User Id=PRODVIEW;Password=Prod-View-OTM-2024;"; 
+        OracleConnection conn = new OracleConnection(connStr);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /*      SUB PROGRAM     */
+        void viewGridData()
+        {
+            try
+            {
+                conn.Open();
+                //OracleCommand cmd = conn.CreateCommand();
+                //cmd.CommandText = "select * from otm.TCO_COMPANY";
+                //cmd.CommandType = CommandType.Text;
+
+                OracleCommand cmd = new OracleCommand("select * from otm.TCO_COMPANY",conn);
+
+                OracleDataAdapter da = new OracleDataAdapter(cmd);
+                //DataSet ds = new DataSet();
+                //da.Fill(ds);
+                //dataGridView1.DataSource = ds.Tables[0];
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x + "");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        void ClearTextbox()
+        {
+            tbID.Clear();
+            tbRegNo.Clear();
+            tbStudentName.Clear();
+            tbAddress.Clear();
+        }
+        void Sql_Command(String SqlCmd)
+        {
+            try
+            {
+                conn.Open();
+
+                OracleDataAdapter da = new OracleDataAdapter(SqlCmd, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                MessageBox.Show("Command Successfully!");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x + "");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /*    END SUB PROGRAM   */
+        private void FormMySQL_Load(object sender, EventArgs e)
+        {
+            //viewGridData();
+        }
+
+        private void btView_Click(object sender, EventArgs e)
+        {
+            viewGridData();
+        }
+
+        private void btInsert_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbID.Text))
+            {
+                String insert = "insert into student (Id,RegNo,student,address) value('"
+                    + tbID.Text + "','" + tbRegNo.Text + "','" + tbStudentName.Text + "','" + tbAddress.Text + "' )";
+                Sql_Command(insert);
+                //For datagrid
+                viewGridData();
+                ClearTextbox();
+            }
+
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            String update = "update student set Regno = '" + tbRegNo.Text + "',student='" + tbStudentName.Text + "',address= '"
+                    + tbAddress.Text + "' where id =" + tbID.Text + " ";
+            Sql_Command(update);
+            //For datagrid
+            viewGridData();
+            ClearTextbox();
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+
+            String delete = "delete from student where id ='" + tbID.Text + "';";
+            Sql_Command(delete);
+            //For datagrid
+            viewGridData();
+            ClearTextbox();
+        }
+
+    }
+}
